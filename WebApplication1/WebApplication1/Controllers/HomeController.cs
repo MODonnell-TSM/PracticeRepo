23a6715df178481fabd72f781c42d8bf399ac556
MODonnell-TSM <50869725+MODonnell-TSM@users.noreply.github.com>
2019-05-21 20:48:09 +0000

    Web Application V2

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        ObjectCache cache = MemoryCache.Default;
+        List<Customer> customers;
+
+        public HomeController()
+        {
+            customers = cache["customers"] as List<Customer>;
+            if (customers == null)
+            {
+                customers = new List<Customer>();
+            }
+        }
+        public void SaveCache()
+        {
+            cache["customers"] = customers;
+        }
         public ActionResult Index()
         {
             return View();
@@ -16,7 +33,7 @@ namespace WebApplication1.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            ViewBag.MySuperProperty = "This is another example";
             return View();
         }
 
@@ -26,5 +43,36 @@ namespace WebApplication1.Controllers
 
             return View();
         }
+
+        public ActionResult ViewCustomer(Customer PostedCustomer)
+        {
+            Customer customer = new Customer();
+
+            customer.Id = Guid.NewGuid().ToString();
+            customer.Name = PostedCustomer.Name;
+            customer.Telephone = PostedCustomer.Telephone;
+
+            return View(customer);
+        }
+
+        public ActionResult AddCustomer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCustomer(Customer customer)
+        {
+            customer.Id = Guid.NewGuid().ToString();
+            customers.Add(customer);
+            SaveCache();
+
+            return RedirectToAction("CustomerList");
+        }
+
+        public ActionResult CustomerList()
+        {
+            return View(customers);
+        }
     }
-}
+}  
